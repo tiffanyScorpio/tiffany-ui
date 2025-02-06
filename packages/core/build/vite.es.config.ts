9b0f29e6fdde1bfef599571f78cc8534d677b328
liuxiaodan <liuxiaodan@cyriex.com>
2025-02-06 16:25:42 +0800

    :tada: remove build-hook

--- packages/core/build/vite.es.config.ts
+++ packages/core/build/vite.es.config.ts
@@ -1,133 +1,72 @@
+/*
+ * @Description:
+ * @Author: liuxiaodan
+ * @FilePath: \packages\core\build\vite.es.config.ts
+ * @Date: 2025-01-19 14:26:08
+ */
 import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
+
 import { readdirSync, readdir } from "fs";
 import { resolve } from "path";
 import { defer, delay, filter, map, includes } from "lodash-es";
-import { visualizer } from "rollup-plugin-visualizer";
-import { hooksPlugin as hooks } from "@eric-ui/vite-plugins";
-import shell from "shelljs";
-
-import vue from "@vitejs/plugin-vue";
-import dts from "vite-plugin-dts";
-import terser from "@rollup/plugin-terser";
-
-const TRY_MOVE_STYLES_DELAY = 750 as const;
-
-const isProd = process.env.NODE_ENV === "production";
-const isDev = process.env.NODE_ENV === "development";
-const isTest = process.env.NODE_ENV === "test";
 
 function getDirectoriesSync(basePath: string) {
-  const entries = readdirSync(basePath, { withFileTypes: true });
+	const entries = readdirSync(basePath, { withFileTypes: true });
 
-  return map(
-    filter(entries, (entry) => entry.isDirectory()),
-    (entry) => entry.name
-  );
-}
-
-function moveStyles() {
-  readdir("./dist/es/theme", (err) => {
-    if (err) return delay(moveStyles, TRY_MOVE_STYLES_DELAY);
-    defer(() => shell.mv("./dist/es/theme", "./dist"));
-  });
+	return map(
+		filter(entries, (entry) => entry.isDirectory()),
+		(entry) => entry.name
+	);
 }
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    visualizer({
-      filename: "dist/stats.es.html",
-    }),
-    dts({
-      tsconfigPath: "../../tsconfig.build.json",
-      outDir: "dist/types",
-    }),
-    terser({
-      compress: {
-        sequences: isProd,
-        arguments: isProd,
-        drop_console: isProd && ["log"],
-        drop_debugger: isProd,
-        passes: isProd ? 4 : 1,
-        global_defs: {
-          "@DEV": JSON.stringify(isDev),
-          "@PROD": JSON.stringify(isProd),
-          "@TEST": JSON.stringify(isTest),
-        },
-      },
-      format: {
-        semicolons: false,
-        shorthand: isProd,
-        braces: !isProd,
-        beautify: !isProd,
-        comments: !isProd,
-      },
-      mangle: {
-        toplevel: isProd,
-        eval: isProd,
-        keep_classnames: isDev,
-        keep_fnames: isDev,
-      },
-    }),
-    hooks({
-      rmFiles: [
-        "./dist/es",
-        "./dist/theme",
-        "./dist/types",
-        "./dist/stats.es.html",
-      ],
-      afterBuild: moveStyles,
-    }),
-  ],
-  build: {
-    outDir: "dist/es",
-    minify: false,
-    cssCodeSplit: true,
-    sourcemap: !isProd,
-    lib: {
-      entry: resolve(__dirname, "../index.ts"),
-      name: "EricUI",
-      fileName: "index",
-      formats: ["es"],
-    },
-    rollupOptions: {
-      external: [
-        "vue",
-        "@fortawesome/fontawesome-svg-core",
-        "@fortawesome/free-solid-svg-icons",
-        "@fortawesome/vue-fontawesome",
-        "@popperjs/core",
-        "async-validator",
-      ],
-      output: {
-        assetFileNames: (chunkInfo) => {
-          if (chunkInfo.name === "style.css") {
-            return "index.css";
-          }
-          if (
-            chunkInfo.type === "asset" &&
-            /\.(css)$/i.test(chunkInfo.name as string)
-          ) {
-            return "theme/[name].[ext]";
-          }
-          return chunkInfo.name as string;
-        },
-        manualChunks(id) {
-          if (includes(id, "node_modules")) return "vendor";
+	plugins: [
+		vue(),
+		dts({
+			tsconfigPath: "../../tsconfig.build.json",
+			outDir: "dist/types",
+		}),
+	],
+	build: {
+		outDir: "dist/es",
+		lib: {
+			entry: resolve(__dirname, "../index.ts"),
+			name: "CyriexUI",
+			fileName: "index",
+			formats: ["es"],
+		},
+		rollupOptions: {
+			external: ["vue", "element-plus", "@element-plus/icons-vue", "async-validator", "@popperjs/core"],
+			output: {
+				exports: "named",
+				globals: {
+					vue: "Vue",
+				},
+				// 设置静态资源文件的命名模式
+				assetFileNames: (chunkInfo) => {
+					if (chunkInfo.name === "style.css") {
+						return "index.css";
+					}
+					return chunkInfo.name as string;
+				},
+				// es打包分包设置
+				manualChunks(id) {
+					// node_modules:第三方引用的库命名为vendor
+					if (includes(id, "node_modules")) return "vendor";
 
-          if (includes(id, "/packages/hooks")) return "hooks";
+					if (includes(id, "/packages/hooks")) return "hooks";
 
-          if (
-            includes(id, "/packages/utils") ||
-            includes(id, "plugin-vue:export-helper")
-          )
-            return "utils";
+					if (includes(id, "/packages/utils") || includes(id, "plugin-vue:export-helper")) return "utils";
 
-          for (const item of getDirectoriesSync("../components")) {
-            if (includes(id, `/packages/components/${item}`)) return item;
-          }
-        },
-      },
-    },
-  },
+					// 自定义组件的分包
+					// 遍历自定义组件的名字，如果和/packages/components/中的文件夹名字相匹配，会生成同名文件
+					for (const item of getDirectoriesSync("../components")) {
+						if (includes(id, `/packages/components/${item}`)) return item;
+					}
+				},
+			},
+		},
+	},
 });
--- packages/hooks/use-size.ts
+++ packages/hooks/use-size.ts
@@ -1,17 +1,17 @@
 /*
- * @Description: 
+ * @Description:
  * @Author: liuxiaodan
  * @FilePath: \packages\hooks\use-size.ts
  * @Date: 2025-01-19 18:42:06
  */
-import {type ComponentSize,componentSizes } from '@cyriex-ui/constants'
+import { type ComponentSize, componentSizes } from "@cyriex-ui/constants";
 
-export const useSizeProp ={
-    type: String,
-    values: componentSizes,
-    required: false,
-  } as const
+export const useSizeProp = {
+	type: String,
+	values: componentSizes,
+	required: false,
+} as const;
 
-  export const useSizeProps = {
-    size: useSizeProp,
-  }
+export const useSizeProps = {
+	size: useSizeProp,
+};
