41e29be36b037aeba2a33117ac78194942d3375f
liuxiaodan <liuxiaodan@cyriex.com>
2025-02-08 17:16:35 +0800

    修改路径

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -3,7 +3,7 @@ import { type App } from "vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
-import TiffanyUI from "tiffany-ui/index";
+import TiffanyUI from "tiffany-ui";
 import { ElementPlusContainer } from "@vitepress-demo-preview/component";
 
 import "@vitepress-demo-preview/component/dist/style.css";
